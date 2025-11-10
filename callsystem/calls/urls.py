1bd5affa4d3405d41c2838815082b7aa7f659f64
AldaAtitus <1137226@atitus.edu.br>
2025-11-10 13:59:33 -0300

    Ajustes1

--- callsystem/calls/urls.py
+++ callsystem/calls/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.call_list, name='call_list'),
-    path('add/', views.add_call, name='add_call'),
-    path('complete/<int:call_id>/', views.complete_call, name='complete_call'),
-    path('delete/<int:call_id>/', views.delete_call, name='delete_call'),
+    path("", views.call_list, name="call_list"),  # rota raiz do app
+    path("add/", views.add_call, name="add_call"),
+    path("<int:call_id>/complete/", views.complete_call, name="complete_call"),
+    path("<int:call_id>/delete/", views.delete_call, name="delete_call"),
 ]
